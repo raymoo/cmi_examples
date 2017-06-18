c09e2da0797b2458537d46c12314e49e43b07316
Raymoo <uguu@installgentoo.com>
2017-06-17 23:51:55 -0700

    CMI registration examples

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
 
 dofile(modpath .. "implementation.lua")
+dofile(modpath .. "showdamage.lua")
---
+++ showdamage.lua
@@ -0,0 +1,15 @@
+cmi.register_on_punchmob(function(mob, hitter, tflp, toolcaps, dir, damage)
+	if hitter and hitter:is_player() then
+		local pname = hitter:get_player_name()
+		local id = cmi.get_uid(mob)
+		minetest.chat_send_all(id .. " got hit by " .. pname .. "!")
+	end
+end)
+
+cmi.register_on_diemob(function(mob, cause)
+	if cause.type == "punch" and cause.puncher and cause.puncher:is_player() then
+		local id = cmi.get_uid(mob)
+		local pname = cause.puncher:get_player_name()
+		minetest.chat_send_all(id .. " was killed by " .. pname .. "!")
+	end
+end)
